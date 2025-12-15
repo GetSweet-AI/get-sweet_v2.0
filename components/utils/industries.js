636e22941788df783ca8aa4d33773335717cdcf5
chill-imax <albrito1509@gmail.com>
2025-12-14 22:15:55 -0400

    `feat: updated various components with new features and improvements`

---
+++ components/utils/industries.js
@@ -0,0 +1,14 @@
+export const INDUSTRIES = [
+  "Restaurants",
+  "E-commerce",
+  "Consulting",
+  "Software Development",
+  "Digital Marketing",
+  "Health & Wellness",
+  "Education",
+  "Real Estate",
+  "Financial Services",
+  "Transportation / Logistics",
+  "Tourism & Hospitality",
+  "Other",
+];
